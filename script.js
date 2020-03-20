f3c0610bf29bfda648aaa3bd3784ae044ac10e9c
Shachar Buda <shacharbuda27@gmail.com>
2020-03-20 16:19:53 +0200

    moment

--- script.js
+++ script.js
@@ -664,11 +664,19 @@ const mock = {
 		const TOTAL_CASES_ID = "total";
 		const TODAY_CASES_ID = "today";
 		const DEATHS_ID = "deaths";
+		const TIME_ID = "time-since-report";
 		
 		$(`#${TOTAL_CASES_ID}`).text(items.cases);
 
 		$(`#${TODAY_CASES_ID}`).text(items.todayCases);
 		
 		$(`#${DEATHS_ID}`).text(items.deaths);
+
+		const reportTime = items.timestamp;
+
+		moment.locale('he');
+		const fromNowText = moment(reportTime).fromNow();
+
+		$(`#${TIME_ID}`).text(fromNowText);
 	}
 	
